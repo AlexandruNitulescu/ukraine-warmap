18d51c1042f310f37359f0d631e9c1aed00258ac
AlexandruNitulescu <Alexandru.florin.nitulescu@gmail.com>
2022-02-27 04:58:58 +0100

    fixed the clock and tested out a new isolated map

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 Ukraine's latitude is 48.3794° N, and the country's longitude is 31.1656° E.
 https://latitude.to/map/ua/ukraine
+
+https://docs.mapbox.com/help/tutorials/style-single-country/#step-6-add-country-borders
--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 mapboxgl.accessToken ="pk.eyJ1IjoiYWFhbGV4eiIsImEiOiJjbDA0ZWZoemEwZ2t3M2RwZGY3NjliZW85In0.4HgWBp3aAJy-3SJ4HsrAjA";
 const map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v11',
+    style: 'mapbox://styles/aaalexz/cl04p5979002214ml7qv9mhis',
     center: [31, 48], // start pos [lng, lat]
     zoom: 6
   });
+
+
+
 
   function clockUpdate(){
 
@@ -18,14 +21,23 @@ const map = new mapboxgl.Map({
         min = datum.getMinutes(),
         sec = datum.getSeconds();
 
+        if(hou == 24){
+            hou=0;
+        }
     
+        Number.prototype.pad = function(digits){
+            for(var n= this.toString(); n.length < digits; n = 0 + n);
+            return n;
+        }
+        
     var months = ["January", "February","March", "April", "May", "June", "July","August", "September", "October", "November", "December"];
     var weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     var ids = ["veckodag", "manad", "dag", "ar", "timme", "minut", "sekund"];
-    var values = [weekday[dname], months[mo], dnum, yr, hou, min, sec];
+    var values = [weekday[dname], months[mo], dnum, yr, hou.pad(2), min.pad(2), sec.pad(2)];
     for(var i=0; i< ids.length; i++)       
     document.getElementById(ids[i]).firstChild.nodeValue= values[i];
   
+
 }
 
   function clockInitialize(){
