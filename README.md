7c55d80f117770ce36ffdd4e1ef38dfde16cab37
AlexandruNitulescu <Alexandru.florin.nitulescu@gmail.com>
2022-02-27 02:46:17 +0100

    Added clock and a navbar

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # ukraine-warmap
+
+Ukraine's latitude is 48.3794° N, and the country's longitude is 31.1656° E.
+https://latitude.to/map/ua/ukraine
--- main.js
+++ main.js
@@ -2,6 +2,33 @@ mapboxgl.accessToken ="pk.eyJ1IjoiYWFhbGV4eiIsImEiOiJjbDA0ZWZoemEwZ2t3M2RwZGY3Nj
 const map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v11',
-    center: [-74.5, 40],
-    zoom: 9
+    center: [31, 48], // start pos [lng, lat]
+    zoom: 6
   });
+
+  function clockUpdate(){
+
+    var datum = new Date();
+    
+    var dname = datum.getDay(),
+        mo = datum.getMonth(),
+        dnum = datum.getDate(),
+        yr = datum.getFullYear(),
+        hou = datum.getHours(),
+        min = datum.getMinutes(),
+        sec = datum.getSeconds();
+
+    
+    var months = ["January", "February","March", "April", "May", "June", "July","August", "September", "October", "November", "December"];
+    var weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    var ids = ["veckodag", "manad", "dag", "ar", "timme", "minut", "sekund"];
+    var values = [weekday[dname], months[mo], dnum, yr, hou, min, sec];
+    for(var i=0; i< ids.length; i++)       
+    document.getElementById(ids[i]).firstChild.nodeValue= values[i];
+  
+}
+
+  function clockInitialize(){
+      clockUpdate();
+      window.setInterval("clockUpdate()",1);
+  }
